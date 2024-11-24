d8f47cd80434eb457710fd9349be84bb0de46d31
Hyungsoo Lim <hyungsoolim0@gmail.com>
2024-11-25 00:31:55 +0900

    aerospace setting added

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -5,14 +5,17 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+-- font ===================================================
 config.font = wezterm.font('D2CodingLigature Nerd Font')
 config.font_size = 13
 
+-- window =================================================
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+config.window_decorations = 'RESIZE'
 -- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
+
 -- and finally, return the configuration to wezterm
 return config
-
