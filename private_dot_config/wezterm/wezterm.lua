9e252b6622b59b3697a6b7833b99d93da4817c4d
Hyungsoo Lim <hyungsoolim0@gmail.com>
2024-11-25 17:43:09 +0900

    🔧 chore: wezterm pane managing script added

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -13,9 +13,115 @@ config.font_size = 13
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 config.window_decorations = 'RESIZE'
+config.use_fancy_tab_bar = true
+-- config.hide_tab_bar_if_only_one_tab = true
+
 -- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
+-- tmux like settings ====================================
+wezterm.on('update-right-status', function(window, pane)
+    local leader = ""
+    if window:leader_is_active() then
+        leader = "LEADER"
+        -- else
+        --     window.set_cursor_color("#89b4fa")
+    end
+    window:set_right_status(leader)
+end)
+
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+config.keys = {
+    {
+        mods = "LEADER",
+        key = "-",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+    },
+    {
+        mods = "LEADER",
+        key = "=",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+    },
+    {
+        mods = "LEADER",
+        key = "w",
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    {
+        mods = "LEADER",
+        key = "m",
+        action = wezterm.action.TogglePaneZoomState,
+    },
+    {
+        mods = "LEADER",
+        key = "Space",
+        action = wezterm.action.RotatePanes "Clockwise",
+    },
+    {
+        mods = "LEADER",
+        key = "0",
+        action = wezterm.action.PaneSelect {
+            mode = "SwapWithActive"
+        }
+    },
+    {
+        mods = "LEADER",
+        key = "h",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        mods = "LEADER",
+        key = "j",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        mods = "LEADER",
+        key = "k",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        mods = "LEADER",
+        key = "l",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+}
+
+local function get_current_working_dir(tab)
+    local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+    local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+    return current_dir == HOME_DIR and "." or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+    local has_unseen_output = false
+    if not tab.is_active then
+        for _, pane in ipairs(tab.panes) do
+            if pane.has_unseen_output then
+                has_unseen_output = true
+                break
+            end
+        end
+    end
+
+    local cwd = wezterm.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Text = get_current_working_dir(tab) },
+    })
+
+    local title = string.format(" [%s] %s", tab.tab_index + 1, cwd)
+
+    if has_unseen_output then
+        return {
+            { Foreground = { Color = "#cba6f7" } },
+            { Text = title },
+        }
+    end
+
+    return {
+        { Text = title },
+    }
+end)
 
 -- and finally, return the configuration to wezterm
 return config
