faf1f4cb2e7a18bb39a0f9a50012d12ed1598187
hyungsoolim <hyungsoolim0@gmail.com>
2026-04-08 17:47:44 +0900

    gruvbox

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -59,25 +59,57 @@ return {
   --   end,
   -- },
   -- github theme ------------------------------------------------------
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   name = "github-theme",
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require("github-theme").setup({
+  --       options = {
+  --         dim_inactive = true,
+  --         styles = {
+  --           comments = "italic",
+  --           -- keywords = "bold",
+  --           types = "italic",
+  --           functions = "italic",
+  --         },
+  --       },
+  --     })
+  --
+  --     vim.cmd("colorscheme github_dark_dimmed")
+  --   end,
+  -- },
   {
-    "projekt0n/github-nvim-theme",
-    name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("github-theme").setup({
-        options = {
-          dim_inactive = true,
-          styles = {
-            comments = "italic",
-            -- keywords = "bold",
-            types = "italic",
-            functions = "italic",
-          },
-        },
-      })
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = true,
+      transparent_mode = false,
+    },
+    config = function(_, opts)
+      require("gruvbox").setup(opts)
 
-      vim.cmd("colorscheme github_dark_dimmed")
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 }
