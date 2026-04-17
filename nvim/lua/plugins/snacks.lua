287a7aee84425a8cb73c76f56ac3103d05320c2e
hyungsoolim <hyungsoolim0@gmail.com>
2026-04-17 12:36:30 +0900

    feat: new nvim plugins

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -8,8 +8,38 @@ return {
             hidden = true,
             ignored = true,
             exclude = { ".git", "node_modules", "__pycache__" },
+            win = {
+              list = {
+                keys = {
+                  ["<c-v>"] = false,
+                  ["<c-s>"] = false,
+                  ["|"] = "edit_vsplit",
+                  ["-"] = "edit_split",
+                },
+              },
+            },
           },
         },
+      },
+    },
+    keys = {
+      -- default keymap disable
+      { "<leader>e", false },
+      { "<leader>E", false },
+      -- new keymap
+      {
+        "<leader>ee",
+        function()
+          Snacks.explorer()
+        end,
+        desc = "Explorer (root dir)",
+      },
+      {
+        "<leader>eE",
+        function()
+          Snacks.explorer({ cwd = false })
+        end,
+        desc = "Explorer (cwd)",
       },
     },
   },
